e101e0f1399cb66e5024a094f6ba89a8f73803a0
simbastart001 <dr.simbastart@gmail.com>
2025-04-01 01:47:04 +0200

    Initial project setup Install necessary dependencies

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
