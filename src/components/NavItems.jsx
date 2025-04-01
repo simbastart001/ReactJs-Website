187304e827fb77cd9dbf92c4565740cd0a17acf1
simbastart001 <dr.simbastart@gmail.com>
2025-04-01 09:29:54 +0200

    Add searchBar & functionality

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -44,52 +44,50 @@ const NavItems = () => {
                   <img src={logo} alt="" />
                 </Link>
               </div>
-              {/* menu area */}
-              <div className="menu-area">
-                <div className="menu">
-                  <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
-                    <li>
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                      <Link to="/shop">Shop</Link>
-                    </li>
-                    <li>
-                      <Link to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                      <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                      <Link to="/contact">Contact</Link>
-                    </li>
-                  </ul>
-                </div>
-                {/* sign up & login */}
-                <Link to="/signup" className="lab-btn me-3 d-none d-md-block">
-                  Create Account
-                </Link>
-                <Link to="/login" className="d-none d-md-block">
-                  Log in
-                </Link>
-                {/* menu toggler  */}
-                <div
-                  onClick={() => setMenuToggle(!menuToggle)}
-                  className={`header-bar d-lg-none ${
-                    menuToggle ? "active" : ""
-                  }`}
-                >
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-                {/* social icon */}
-                <div
-                  className="ellepsis-bar d-md-none"
-                  onClick={() => setSocialToggle(!socialToggle)}
-                >
-                  <i className="icofont-info-square"></i>
-                </div>
+            </div>
+            {/* menu area */}
+            <div className="menu-area">
+              <div className="menu">
+                <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/shop">Shop</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+              {/* sign up & login */}
+              <Link to="/signup" className="lab-btn me-3 d-none d-md-block">
+                Create Account
+              </Link>
+              <Link to="/login" className="d-none d-md-block">
+                Log in
+              </Link>
+              {/* menu toggler  */}
+              <div
+                onClick={() => setMenuToggle(!menuToggle)}
+                className={`header-bar d-lg-none ${menuToggle ? "active" : ""}`}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              {/* social icon */}
+              <div
+                className="ellepsis-bar d-md-none"
+                onClick={() => setSocialToggle(!socialToggle)}
+              >
+                <i className="icofont-info-square"></i>
               </div>
             </div>
           </div>
