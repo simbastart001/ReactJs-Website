632f6051f41ed28f2f616d0107b94f08670898b8
simbastart001 <dr.simbastart@gmail.com>
2025-04-02 23:58:11 +0200

    Add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavItems from "./components/NavItems";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavItems />
       <Outlet />
+      <Footer />
     </>
   );
 }
