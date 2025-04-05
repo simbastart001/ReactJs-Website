a709f4c89c643dce42e5b8a5abf527f0d33d1b6c
simbastart001 <dr.simbastart@gmail.com>
2025-04-05 13:06:21 +0200

    Add the Products display section on Shop page

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import PageBanner from "../components/PageBanner";
+import Data from "../products.json";
+import ProductCards from "./ProductCards";
+
+const showResults = "Showing 01-12 of 139 results";
 
 const Shop = () => {
+  const [GridList, setGridList] = useState(true);
+  const [products, setProducts] = useState(Data);
+
   return (
     <div>
       <PageBanner title="Our Shop Page" currentPage="Shop" />
+      {/* Shop Main Content */}
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                {/* layout & title */}
+                <div className="shop-title d-flex flex-warp justify-content-between">
+                  <p>{showResults}</p>
+                  <div
+                    className={`product-view-mode ${
+                      GridList ? "gridActive" : "listActive"
+                    }`}
+                  >
+                    <a className="grid" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-ghost"></i>
+                    </a>
+                    <a className="list" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-listine-dots"></i>
+                    </a>
+                  </div>
+                </div>
+                {/* other */}
+                <div>
+                  <ProductCards GridList={GridList} products={products} />
+                </div>
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">right side</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
